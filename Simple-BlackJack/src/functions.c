f076a263f608a617551940633b27d043e7e98ca8
Flapjacck <plkelly333@me.com>
2024-10-03 20:02:20 -0400

    Addded game_start function

--- Simple-BlackJack/src/functions.c
+++ Simple-BlackJack/src/functions.c
@@ -14,10 +14,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//function to start the game
+int game_start() {
+	int cash = 0;
+	printf("Welcome to Simple Blackjack!\n");
+	printf("How much cash would you like to start with?:");
+	scanf("%d", &cash);
+	return cash;
+}
+
 //function to get the bet amount
 int bet(int cash) {
 	int bet = 0;
-	printf("How much would you like to bet? ");
+	printf("How much would you like to bet?:");
 	scanf("%d", &bet);
 	if (bet > cash) {
 		printf("You don't have that much money!\n");
--- Simple-BlackJack/src/functions.h
+++ Simple-BlackJack/src/functions.h
@@ -18,10 +18,13 @@
 // Constants.
 
 // Prototypes.
+//start of game, asks user for starting cash amount
+//return cash
+int game_start();
 
 //gets the players bet amount
-//@param cash - amount of cash player has
-//@return remaining - cash
+//param cash - amount of cash player has
+//return remaining - cash
 int bet(int cash);
 
 #endif /* FUNCTIONS_H_ */
