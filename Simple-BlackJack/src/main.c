acd1ff4329e6c0079ef0eaf46dfcdce7cd7d6c00
Flapjacck <plkelly333@me.com>
2024-10-03 22:48:20 -0400

    Updated main.c

--- Simple-BlackJack/src/main.c
+++ Simple-BlackJack/src/main.c
@@ -18,13 +18,15 @@
 
 //variables
 
-int cash = 100;
 int dealer_total = 0;
 int player_total = 0;
 
 int main(int argc, char *argv[]) {
 	setbuf(stdout, NULL);
 
+	//game start
+	int cash = game_start();
+	printf("You have $%d\n", cash);
 	//user input for number of decks
 	int num_decks = 0;
 	printf("Enter the number of decks to use: ");
@@ -32,12 +34,18 @@ int main(int argc, char *argv[]) {
 	Card decks[num_decks * 52];
 
 	init_decks(decks, num_decks);
-	printf("Multiple decks initialized:\n");
+	shuffle_deck(decks, num_decks * 52);
+	printf("Multiple decks initialized and shuffled:\n");
 	print_deck(decks, num_decks * 52);
-	/*
-	 shuffle_deck(decks, num_decks * 52);
-	 printf("\nShuffled decks:\n");
-	 print_deck(decks, num_decks * 52);
-	 */
+
+	while (cash > 0) {
+		//bet
+		int bet_amount = bet(cash);
+		cash -= bet_amount;
+
+		//deal cards to player and dealer
+
+	}
+
 	return 0;
 }
